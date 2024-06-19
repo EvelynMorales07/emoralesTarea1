ad9c9585eb99ad2c1614964c2bb1e209a2239a8a
EvelynMorales07 <evemorales.ar@gmail.com>
2024-06-19 16:13:52 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace emoralesTarea1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.vHome();
+        }
+    }
+}
